182f26a71ea0b51962ae4d12e597eeb61c5b04bf
f_estaun <f.estaun@gmail.com>
2015-10-29 23:45:43 +0100

    -Ejercicio prioridades resuelto

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -7,20 +7,27 @@ class elementos extends Thread {
     public elementos(char c) {
         this.elemento = c;
     }
-
+    /*
+    * Metodo run() de la clase derivada de Thread, donde se indica el funcionamiento del metodo de conteo de elementos.
+    * Se añaden numeros secuencialmente incrementados  al valor del elemento inicial, y se imprime por pantalla.
+    * */
     public void run() {
+        char extra;
         for (int i = 0; i < 10; i++) {
-            System.out.println((char) (elemento + i));
+            extra = (char) (elemento + i);
+            System.out.println(extra);
         }
     }
-
 }
+/*Clase princpipal en la que se inician los hilos y se establecen prioridades
+* Utilizo "MIN_PRIORITY" y "MAX_PRIORITY porque pense que te referias a eso con constantes
+* Estos valores de prioridad varian entre 1 y 10 asi que podria utilizar 1 y 10 para este ejercicio*/
 public class Ejercicio2 {
     public static void main(String[] args) {
         Thread t1 = new elementos('a');
         Thread t2 = new elementos('0');
-        t2.setPriority(10);
-        t1.setPriority(1);
+        t2.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
         t1.start();
         t2.start();
     }
--- src/Prioridad.java
+++ src/Prioridad.java
@@ -1,13 +1,14 @@
 
-
+//clase CounterThread que extiende de Thread, sera la clase donde describiremos el codigo a utilizar
 class CounterThread extends Thread {
+    //Usamos esta variable para diferenciar los dos hilos a la hora de ejecutar el programa
     String name;
 
     public CounterThread(String name) {
         super();
         this.name = name;
     }
-
+    //Metodo que incrementa el valor de un contador hasta un limite de 1000, mostrando cada numero por pantalla
     public void run() {
         int count = 0;
         while (true) {
@@ -16,19 +17,34 @@ class CounterThread extends Thread {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if (count == 1000) count = 0;
+            if (count == 100) count = 0;
             System.out.println(name +":"+count++);
         }
     }
 }
-
+/*
+* Nuestra clase main en la que declaramos ambos hilos y les establecemos una prioridad determinada.
+* En este caco asignamos al t1 l a prioridad mas alta (10) y al t2 la mas baja (1).
+* En teoría puesto que hemos asignado asi las prioridades, el thread 1 se deberia ejecutar primero, y sera el primero
+* en resetea el contador una vez el llegue a su limite
+* En la practica observamos que la diferencia de cuando terminan es inapreciable (en tiempo), y se muestran los
+* los resultados de ambos hilos intercalados por consola.
+* */
 public class Prioridad {
     public static void main(String[] args) {
         CounterThread thread1 = new CounterThread("thread1");
-        thread1.setPriority(10);
+        thread1.setPriority(1);
         CounterThread thread2 = new CounterThread("thread2");
-        thread2.setPriority(1);
+        thread2.setPriority(10);
         thread2.start();
         thread1.start();
     }
 }
+/*
+* Si se intercambia la prioridad de ambos hilos no se puede apreciar diferencia, ya que la ejecucion de ambos hilos
+* sigue realizandose intercaladamente.
+* El hecho de que sea irrelevante se debe principalmente a que esta muy relacionado con el procesador desde el que se
+* ejecute.
+* SI lo ejecutaramos desde un ordenador muy antiguo y lento, igual podriamos notar alguna diferencia significativa,
+* pero con los resultados actuales, la impresion siempre se ejecuta de manera similar (intercalada)
+* */
